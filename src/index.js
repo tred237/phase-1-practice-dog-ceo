c2df409c6c453ea8d013fd8c31a8e69d94305a31
trevor dewindt <trevor.dewindt@gmail.com>
2022-08-16 20:29:45 -0600

    commit tree 9f6c58b004ee3b9d51fd9b9fa0c4b3a4bfc551d2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,72 @@
-console.log('%c HI', 'color: firebrick')
+const init = () => {
+
+    console.log('%c HI', 'color: firebrick');
+
+    dogImages();
+    dogBreeds();
+
+    function dogImages(){
+        const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+        fetch(imgUrl)
+        .then(res => res.json())
+        .then(data => {
+            const dogImageContainer = document.getElementById('dog-image-container'); 
+            data.message.forEach(element => {
+                const image = document.createElement('img');
+                image.src = element;
+                image.alt = 'dog-picture';
+                dogImageContainer.appendChild(image);
+            })
+        });
+    }
+
+    function dogBreeds(){
+        const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+
+        fetch(breedUrl)
+        .then(res => res.json())
+        .then(data => {
+            const dogBreedsList = document.getElementById('dog-breeds');
+            for(breed in data.message){
+                if (data.message[breed].length > 0) {
+                    data.message[breed].forEach(element => {
+                        const li = document.createElement('li');
+                        li.className = 'dog-breed';
+                        li.id = `breed-${breed}-${element}`;
+                        li.textContent = `${breed} ${element}`;
+                        dogBreedsList.appendChild(li);
+                    })
+                } else {
+                    const li = document.createElement('li');
+                    li.className = 'dog-breed';
+                    li.id = `breed-${breed}`;
+                    li.textContent = breed;
+                    dogBreedsList.appendChild(li);
+                }
+            }
+        })
+        .then(() => {
+            const dogBreedsListElements = document.querySelectorAll('.dog-breed');
+            const dropDown = document.getElementById('breed-dropdown');
+
+            dogBreedsListElements.forEach(element => {
+                element.addEventListener('click', event => event.target.style.color = 'red');
+            })
+            
+            dropDown.addEventListener('change', handleDropDown);
+
+            function handleDropDown(event) {
+                dogBreedsListElements.forEach(element => {
+                    if(!element.textContent.startsWith(event.target.value)) {
+                        element.hidden = 'true'
+                    } else {
+                        element.removeAttribute('hidden');
+                    }
+                })
+            }
+        })
+    }
+}
+
+addEventListener('DOMContentLoaded',init);
